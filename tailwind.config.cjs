94eed6dcae45bf3025bac9f591861d21eb5b9dd0
Abdelwahab Hussein <abdelwahab.huss07@gmail.com>
2023-08-28 02:33:30 +0300

    fix dark class not added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,17 +3,15 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         sans: ['Poppins', ...defaultTheme.fontFamily.sans],
       },
-      colors: {
-        bgContent: '#02070c',
-        hovered: 'rgb(243,244,246)',
-      },
       boxShadow: {
         light: '0 0 30px 2px #002fff57',
+        dark: '0 0 20px 2px #cbd5e1',
       },
     },
   },
